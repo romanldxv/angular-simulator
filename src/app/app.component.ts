4571896a5f89533a6c933285da2fcd4ca8d6be90
Roman Shaymardanov <souiside.future@gmail.com>
2026-01-29 00:36:53 +0500

    use generic & enums

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import './training'
+import { Color } from '../enums/Colors';
+import { Collection } from './collection';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,34 @@ export class AppComponent {
 
   companyName: string = 'румтибет';
 
+  constructor() {
+    saveVisitDate();
+    saveEntryCount();
+  }
+
 }
+
+function isMainColor(color: string): boolean {
+  const mainColors: string[] = Object.values(Color);
+  return mainColors.includes(color);
+}
+
+function saveVisitDate(): void {
+  const todayDate: Date = new Date();
+  localStorage.setItem('lastVisitDate', todayDate.toString());
+}
+
+function saveEntryCount(): void {
+  if (!localStorage.getItem('visitCount')) {
+    localStorage.setItem('visitCount', JSON.stringify(1))
+  } else {
+    let visitCount: number = JSON.parse(localStorage.getItem('visitCount')!);
+    localStorage.setItem('visitCount', JSON.stringify(visitCount + 1));
+  }
+}
+
+const products: string[] = ['apple', 'carrot', 'milk', 'bread'];
+const colors: Color[] = Object.values(Color);
+
+const productCollection: Collection<string> = new Collection(products);
+const colorCollection: Collection<Color> = new Collection(colors);
---
+++ src/app/collection.ts
@@ -0,0 +1,28 @@
+export class Collection<T> {
+
+  items: T[] = [];
+  constructor(items: T[]) {
+    this.items = items;
+  }
+
+  getAll(): T[] {
+    return this.items;
+  }
+
+  getItem(index: number): T {
+    return this.items[index];
+  }
+
+  deleteAll(): void {
+    this.items.length = 0;
+  }
+  
+  delete(index: number): void {
+    this.items.splice(index, 1);
+  }
+
+  setItem(index: number, item: T): void {
+    this.items[index] = item;
+  }
+  
+}

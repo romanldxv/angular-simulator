cbe1017fc72d88ac8665a84bb84a05d7da38b063
soulsxde <souiside.future@gmail.com>
2026-06-01 10:47:07 +0500

    add plural pipe

---
+++ src/app/pipes/word-form.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'plural',
+})
+export class PluralPipe implements PipeTransform {
+
+  transform(number: number, singularWord: string, wordBetweenForms: string, pluralWord: string): string {
+    const remainder = number % 10;
+    const twoDigitNumbers: number[] = [11, 12, 13, 14, 15, 16, 17, 18, 19];
+    
+    if (number == 0 || remainder == 0 || remainder >= 5 || twoDigitNumbers.includes(number)) {
+      return `${number} ${pluralWord}`;
+    }  
+    else if (remainder == 1) {
+      return `${number} ${singularWord}`;
+    }  
+    else {
+      return `${number} ${wordBetweenForms}`;
+    }  
+  }
+
+}

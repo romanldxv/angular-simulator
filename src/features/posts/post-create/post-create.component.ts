47debf90fd6ef6e297f980a02f4572ac726d9f3c
Roman Shaymardanov <souiside.future@gmail.com>
2026-06-17 16:39:02 +0500

    add minimal structure

---
+++ src/features/posts/post-create/post-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  imports: [],
+  templateUrl: './post-create.component.html',
+  styleUrl: './post-create.component.scss',
+})
+export class PostCreateComponent {
+
+}
---
+++ src/features/posts/post-edit-dialog/post-edit-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-edit-dialog',
+  imports: [],
+  templateUrl: './post-edit-dialog.component.html',
+  styleUrl: './post-edit-dialog.component.scss',
+})
+export class PostEditDialogComponent {
+
+}

85f029b9ea7eb995a6014d766507fa0aeef06f82
Roman Shaymardanov <souiside.future@gmail.com>
2026-07-12 00:04:57 +0500

    add admin guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { postResolver } from '../features/posts/post.resolver';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { authGuard } from '../features/auth/auth.guard';
+import { adminGuard } from '../features/auth/admin.guard';
 
 export const routes: Routes = [
   { 
@@ -13,24 +14,34 @@ export const routes: Routes = [
         path: '',
         loadComponent: () => import('./home-page/home-page.component').then(m => m.HomePageComponent)
       },
-      { 
-        path: 'users', 
-        loadComponent: () => import('./users-page/users-page.component').then(m => m.UsersPageComponent)
+      {
+        path: '',
+        canActivate: [adminGuard],
+        children: [
+          { 
+            path: 'users', 
+            loadComponent: () => import('./users-page/users-page.component').then(m => m.UsersPageComponent)
+          },
+          { 
+            path: 'posts', 
+            loadComponent: () => import('../features/posts/posts/posts.component').then(m => m.PostsComponent)
+          },
+          { 
+            path: 'posts/create', 
+            loadComponent: () => import('../features/posts/post-create/post-create.component').then(m => m.PostCreateComponent)
+          },
+          { 
+            path: 'posts/:id', 
+            loadComponent: () => import('../features/posts/post-detail/post-detail.component').then(m => m.PostDetailComponent),
+            resolve: {
+              post: postResolver
+            }
+          }
+        ]
       },
-      { 
-        path: 'posts', 
-        loadComponent: () => import('../features/posts/posts/posts.component').then(m => m.PostsComponent)
-      },
-      { 
-        path: 'posts/create', 
-        loadComponent: () => import('../features/posts/post-create/post-create.component').then(m => m.PostCreateComponent)
-      },
-      { 
-        path: 'posts/:id', 
-        loadComponent: () => import('../features/posts/post-detail/post-detail.component').then(m => m.PostDetailComponent),
-        resolve: {
-          post: postResolver
-        }
+      {
+        path: '404',
+        loadComponent: () => import('./not-found-page/not-found-page.component').then(m => m.NotFoundPageComponent)
       }
     ]
   },
@@ -38,8 +49,12 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('../features/auth/login/login.component').then(m => m.LoginComponent)
   },
-  { 
-    path: '**', 
+  {
+    path: '404',
     loadComponent: () => import('./not-found-page/not-found-page.component').then(m => m.NotFoundPageComponent)
+  },
+  { 
+    path: '**',
+    redirectTo: '404'
   }
 ];
---
+++ src/features/auth/admin.guard.ts
@@ -0,0 +1,16 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { inject } from '@angular/core';
+import { first, map } from 'rxjs';
+import { IAuthUser } from './IAuthUser';
+import { UserRole } from '../../enums/UserRole';
+
+export const adminGuard: CanActivateFn = (route, state) => {
+  const router: Router = inject(Router);
+  const authService: AuthService = inject(AuthService);
+
+  return authService.user$.pipe(
+    first(),
+    map((user: IAuthUser | null) => user?.role === UserRole.ADMIN ? true : router.parseUrl('/404'))
+  );
+};

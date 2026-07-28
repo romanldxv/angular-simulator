48f10b02ba46a846713582e87ead009428a20d28
Roman Shaymardanov <souiside.future@gmail.com>
2026-07-28 16:36:21 +0500

    refactoring code 2

--- src/app/interceptors/log.interceptor.ts
+++ src/app/interceptors/log.interceptor.ts
@@ -22,18 +22,15 @@ export const logInterceptor: HttpInterceptorFn = (
     let responseStatus: number = 0;
     return next(req).pipe(
       tap(
-        (event: HttpEvent<any>) =>
-          event.type === HttpEventType.Response
-            ? (responseStatus = event.status)
-            : responseStatus,
-        (error: HttpErrorResponse) => (responseStatus = error.status),
+        (event: HttpEvent<any>) => event.type === HttpEventType.Response ? responseStatus = event.status : responseStatus,
+        (error: HttpErrorResponse) => responseStatus = error.status
       ),
       finalize(() => {
         console.log(`
-          HTTP method: ${req.method}\n
-          url: ${req.url}\n
-          status request: ${responseStatus}\n
-          response time: ${Date.now() - startedTime} ms\n
+          HTTP method: ${ req.method }\n
+          url: ${ req.url }\n
+          status request: ${ responseStatus }\n
+          response time: ${ Date.now() - startedTime } ms\n
         `);
       }),
     );
